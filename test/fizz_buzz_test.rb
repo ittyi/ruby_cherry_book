5aa65da3a123a57f065f4a51ad14d0db234abcad
ittyi <vioiv.k4@gmail.com>
2021-12-25 06:56:28 +0900

    add testcode

---
+++ test/fizz_buzz_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require_relative '../lib/fizz_buzz'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_bazz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal 'Bazz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'Fizz Bazz', fizz_buzz(15)
+  end
+end
+
+# puts fizz_buzz(1) Fizz Bazz
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(4)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(15)
