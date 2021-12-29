ea3050ec7e6ca73c16e1846df48b2e6c16ecf807
ittyi <vioiv.k4@gmail.com>
2021-12-29 17:51:33 +0900

    定数化

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,8 +1,9 @@
 # 方針：メートル等の単位との比率をハッシュで定義し、
 #       そのハッシュを使って単位を変換する
+UNITS = {m: 1.0, ft: 3.28, in: 39.37}
+
 
 def convert_length(length, from: :m, to: :m)
-  units = {m: 1.0, ft: 3.28, in: 39.37}
 
-  (length / units[from] * units[to]).round(2)
+  (length / UNITS[from] * UNITS[to]).round(2)
 end
