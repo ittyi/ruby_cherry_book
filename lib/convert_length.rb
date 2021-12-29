b16a9f59089c7b7c2a27ab28e9a59ce14df33c56
ittyi <vioiv.k4@gmail.com>
2021-12-29 17:42:20 +0900

    convert_length実装

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,3 +1,8 @@
-def convert_length(length, unit_form, unit_to)
-  39.37
+# 方針：メートル等の単位との比率をハッシュで定義し、
+#       そのハッシュを使って単位を変換する
+
+def convert_length(length, unit_from, unit_to)
+  units = {'m' => 1.0, 'ft' => 3.28, 'in' => 39.37}
+
+  (length / units[unit_from] * units[unit_to]).round(2)
 end
